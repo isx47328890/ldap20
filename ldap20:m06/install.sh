34c6e5fd459a0ea2f30926a76f039bc24580ac41
Mark Stanley Santiago Burgos <mssb71@gmail.com>
2020-10-21 07:42:28 +0200

    ldap20:m06

--- ldap20:m06/install.sh
+++ ldap20:m06/install.sh
@@ -5,7 +5,8 @@ rm -rf /etc/openldap/slapd.d/*
 rm -rf /var/lib/ldap/*
 cp /opt/docker/DB_CONFIG /var/lib/ldap/.
 slaptest -f /opt/docker/slapd.conf -F /etc/openldap/slapd.d
-slapadd -F /etc/openldap/slapd.d -l /opt/docker/tot-edt.org.ldif
+slapadd -F /etc/openldap/slapd.d -l /opt/docker/organitzacio-m06.cat.ldif
+slapadd -F /etc/openldap/slapd.d -l /opt/docker/usuaris-m06.cat.ldif 
 chown -R ldap.ldap /etc/openldap/slapd.d
 chown -R ldap.ldap /var/lib/ldap
 cp /opt/docker/ldap.conf /etc/openldap/ldap.conf

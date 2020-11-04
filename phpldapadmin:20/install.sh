36d13c69ebe76641bd2181352b60c523451ca732
Mark Stanley Santiago Burgos <mssb71@gmail.com>
2020-11-04 12:01:22 +0100

    04/11/2020

--- phpldapadmin:20/install.sh
+++ phpldapadmin:20/install.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# Install phpldapadmin
+# Install phpldapadmin configuration
 
 cp /opt/phpldapadmin/config.php /etc/phpldapadmin/.
 cp /opt/phpldapadmin/phpldapadmin.conf /etc/httpd/conf.d/. 
